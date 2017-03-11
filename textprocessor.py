c457e7dce4326f561f2188d902fab37681709c3c
samvaran <samvaran2best@gmail.com>
2017-03-11 06:15:06 +0000

    added anuvasra and visarga case too

--- textprocessor.py
+++ textprocessor.py
@@ -5,6 +5,8 @@ text = open("samples/kandam_sample.txt","r").read()
 f = codecs.open("samples/kandam_sample.txt", "r", "utf-8")
 #print f.readline()
 
+GURU_LABELS =  ["AA", "AI", "EE", "OO", "II", "AU", "RR", "LL"]
+
 def ganav(desc):
     "gana vibhajana"
     ganas = []
@@ -17,7 +19,7 @@ def ganav(desc):
             ganas.append("I")
         if "VOWEL" in d:
             sign = d.split()[-1]
-            if sign in ["AA", "AI", "EE", "OO", "II"]:
+            if sign in GURU_LABELS:
                 ganas.pop()
                 ganas.append("U")
         if "VIRAMA" in d:
@@ -26,7 +28,9 @@ def ganav(desc):
             ganas.append("U")
             ganas.append("I")
             v = True
-            
+        if ("ANUSVARA" in d) or ("VISARGA" in d):
+            ganas.pop()
+            ganas.append("U")
     return ganas
 
 lines = f.readlines()
