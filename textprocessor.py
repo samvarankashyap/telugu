5eb9ea598a65701b0e5fa605c684fcc59f205930
samvaran <samvaran2best@gmail.com>
2017-03-12 00:08:00 +0000

    Functionized gana vibhajana

--- textprocessor.py
+++ textprocessor.py
@@ -1,47 +1,42 @@
 # -*- coding: utf-8 -*-
 import codecs
 import unicodedata
-text = open("samples/kandam_sample.txt","r").read()
-f = codecs.open("samples/kandam_sample.txt", "r", "utf-8")
-#print f.readline()
 
 GURU_LABELS =  ["AA", "AI", "EE", "OO", "II", "AU", "RR", "LL"]
 
-def ganav(desc):
-    "gana vibhajana"
-    ganas = []
+def gana_vibhajana(padyam):
+    lines = padyam.split("\n")
+    desc = []
     v = False
-    for d in desc:
-        if "LETTER" in d:
-            if v:
-                v = False
+    ganas = []
+    for line in lines:
+        for c in line:
+            if c == " ":
                 continue
-            ganas.append("I")
-        if "VOWEL" in d:
-            sign = d.split()[-1]
-            if sign in GURU_LABELS:
+            unicode_name = unicodedata.name(c)
+            if "LETTER" in unicode_name:
+                if v:
+                    v = False
+                    continue
+                ganas.append("I")
+            if "VOWEL" in unicode_name:
+                label = unicode_name.split()[-1]
+                if label in GURU_LABELS:
+                    ganas.pop()
+                    ganas.append("U")
+            if "VIRAMA" in unicode_name:
+                ganas.pop()
                 ganas.pop()
                 ganas.append("U")
-        if "VIRAMA" in d:
-            ganas.pop()
-            ganas.pop()
-            ganas.append("U")
-            ganas.append("I")
-            v = True
-        if ("ANUSVARA" in d) or ("VISARGA" in d):
-            ganas.pop()
-            ganas.append("U")
-    return ganas
+                ganas.append("I")
+                v = True
+            if ("ANUSVARA" in unicode_name) or ("VISARGA" in unicode_name):
+                ganas.pop()
+                ganas.append("U")
+    return " ".join(ganas)
 
-lines = f.readlines()
-letters = []
-gana = []
-
-desc = []
-for line in lines:
-    print line
-    for word in line.split():
-        desc.extend([ unicodedata.name(c) for c in word])
-    print desc
-
-print ganav(desc)
+text = open("samples/kandam_sample.txt","r").read()
+f = codecs.open("samples/kandam_sample.txt", "r", "utf-8")
+lines = f.read()
+print gana_vibhajana(lines)
+print "".join(lines.split("\n"))
